a87e91548d70edbafeced9d31969a61abde0265b
Bruno Santos Chagas <brunoprimo@gmail.com>
2012-11-06 19:57:04 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiscoitoBruno::Application.config.secret_token = '132fc38417e775fd7639ad3497f1407030304dca0142ef7c41d0a6bb67c13ff452a2dd8b3fa071b830417956ec0110baed517b5d095036ecbc7bd0d0eb94e944'
